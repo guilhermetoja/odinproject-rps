d6649e791f961e484c73809f886e6fa05cf0f34d
Guilherme Toja Camilo <guilherme.toja.camilo@gmail.com>
2024-09-27 15:48:04 -0300

    feat: added ui to the stuff bro

--- rps.js
+++ rps.js
@@ -10,43 +10,13 @@ function getComputerChoice() {
   }
 }
 
-//  for debugging the func
-// console.log(getComputerChoice());
-
-function getPlayerChoice() {
-  try {
-    let validChoice = false;
-
-    do {
-      var playerChoice = prompt(
-        "What is your choice, traveler? Paper, rock or scissors?"
-      ).toLowerCase();
-
-      if (
-        playerChoice == "paper" ||
-        playerChoice == "rock" ||
-        playerChoice == "scissors"
-      ) {
-        validChoice = true;
-      }
-    } while (validChoice == false);
-
-    return playerChoice;
-  } catch (error) {
-    throw new Error(
-      "You did something explendid and the func returned an error. Good."
-    );
-  }
-}
-
-// for debugging
-// console.log(getPlayerChoice());
-
 var playerScore = 0;
+var playerScoreGame = 0;
 var computerScore = 0;
+var computerScoreGame = 0;
 
 function playRound(humanChoice, computerChoice) {
-  switch (humanChoice) {
+  switch (humanChoice.toLowerCase()) {
     case "paper":
       if (computerChoice == "rock") {
         playerScore++;
@@ -94,28 +64,55 @@ function playRound(humanChoice, computerChoice) {
   }
 }
 
-// var humanChoice = getPlayerChoice();
-// var computerChoice = getComputerChoice();
+const body = document.querySelector("body");
+body.setAttribute("style", "margin: 0px; background: #5D4954; ");
 
-// console.log(playRound(humanChoice, computerChoice));
+const container = document.querySelector("#container");
+container.setAttribute(
+  "style",
+  "display: flex; align-content:center; justify-content:center;  background: #E6DBD0; margin: 4rem; flex-direction: column; "
+);
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    console.log("Round " + (i + 1));
-    let humanChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
+const uiGameScore = document.querySelector("#score-game");
+uiGameScore.setAttribute(
+  "style",
+  "font-size: 14px; color: #754F5B; text-align: center; margin: 0.5rem;"
+);
+uiGameScore.textContent = playerScoreGame + " : " + computerScoreGame;
 
-    console.log(playRound(humanChoice, computerChoice));
-    console.log("Next round");
-  }
+const uiMatchScore = document.querySelector("#score-match");
+uiMatchScore.setAttribute(
+  "style",
+  "font-size: 36px; text-align: center; margin: 2rem;"
+);
 
-  if (playerScore > computerScore) {
-    console.log("You are the ultimate winner, friend");
-  } else if (playerScore < computerScore) {
-    console.log("You are a loser. Do it again.");
-  } else {
-    console.log("Tie");
-  }
-}
+const interface = document.querySelector("#interface");
+interface.setAttribute("style", "display: flex; justify-content: center; ");
+uiMatchScore.textContent = playerScore + " : " + computerScore;
 
-playGame();
+const choices = interface.querySelectorAll("button");
+
+choices.forEach((button) => {
+  button.setAttribute(
+    "style",
+    "border: none; width: 4rem; height: 4rem; margin: 1rem; font-size: 14px; border-radius: 50%"
+  );
+  button.addEventListener("click", () => {
+    alert(playRound(button.textContent, getComputerChoice()));
+    if (playerScore == 5) {
+      playerScore = 0;
+      computerScore = 0;
+      playerScoreGame++;
+      uiGameScore.textContent = playerScoreGame + " : " + computerScoreGame;
+      uiMatchScore.textContent = "Player wins!";
+    } else if (computerScore == 5) {
+      playerScore = 0;
+      computerScore = 0;
+      computerScoreGame++;
+      uiGameScore.textContent = playerScoreGame + " : " + computerScoreGame;
+      uiMatchScore.textContent = "Computer wins! Ya loser!";
+    } else {
+      uiMatchScore.textContent = playerScore + " : " + computerScore;
+    }
+  });
+});
